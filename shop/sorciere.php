06d32a914c3a05dd90a122a26c093379ebf0649f
DeshnaDhayan <91412093+DeshnaDhayan@users.noreply.github.com>
2022-02-25 11:02:02 +0100

    Add files via upload

---
+++ shop/sorciere.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<title>Boutique</title>
+<head>
+<link rel="stylesheet" type="text/css" href="shop.css">
+</head>
+<body>
+    <header class="container">
+        <h1>Nos plus beaux gâteaux</h1>
+        
+    </header>
+    <main>
+        <p>... Il y en a pour tous les goûts !</p>
+        <div>
+            <img src="images/sorcière.jpg">
+            <p>Prix : 1€</p>
+            <p>Livraison : 99€ HT</p>
+            <p>Description : Joli gâteau.</p>
+        </div>
+    </main>
+</body>
