194c9b52ec2ba334c2bd1502fb1e4c00c128273d
Mathilde Nanécou <90638817+mathooouuu@users.noreply.github.com>
2022-02-25 10:35:33 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KelseyCake - Page d'accueil</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+    <?php include_once('header.php'); ?>
+        <div class="container">
+
+    
+            <h1>Bienvenue sur notre site de vente de gâteaux !</h1>
+
+       
+        </div>
+
+    <?php include_once('footer.php'); ?>
+</body>
+</html>
