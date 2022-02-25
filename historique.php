2ab0c384f2cc53be7bcfe99343c18f65bfd9954f
do2349 <93477183+do2349@users.noreply.github.com>
2022-02-25 10:43:50 +0100

    Add files via upload

---
+++ historique.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <title>
+            Mon Compte
+        </title>
+    </head>
+    <body>
+        <?php include_once('header.php'); ?>
+        <h1>Mes commandes</h1>
+        <h3>Liste des commandes passées</h3>
+        <?php include_once('footer.php'); ?>
+    </body>
+</html>
